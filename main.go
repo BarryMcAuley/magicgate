80a9892f6c407eda3123958cdfe2fdc4cfd123de
BarryMcAuley <BarryMcAuley92@Gmail.com>
2018-03-06 20:57:15 +0000

    Recording and playback

--- main.go
+++ main.go
@@ -1,21 +1,57 @@
 package magicgate
 
 import (
+	"context"
 	"fmt"
+	"io/ioutil"
 	"net/http"
+
+	"google.golang.org/appengine/urlfetch"
 )
 
 func init() {
 	http.HandleFunc("/", handler)
 }
 
-const content = `<?xml version="1.0" encoding="UTF-8"?>
+const (
+	welcomeMsg = `<?xml version="1.0" encoding="UTF-8"?>
 	<Response>
-		<Say>Hello Barry!</Say>
-		<Play digits="9"></Play>
+		<Say>Hello Barry, how are you?</Say>
+		<Record timeout="5"/>
 	</Response>`
+	echoMsg = `<?xml version="1.0" encoding="UTF-8"?>
+	<Response>
+		<Play>%s</Play>
+	</Response>`
+)
 
 func handler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/xml")
-	fmt.Fprint(w, content)
+
+	rec := r.FormValue("RecordingUrl")
+	if rec == "" {
+		fmt.Fprint(w, welcomeMsg)
+		return
+	}
+
+	fmt.Fprintf(w, echoMsg, rec)
+}
+
+func transcribe(c context.Context, url string) ([]byte, error) {
+	client := urlfetch.Client(c)
+	res, err := client.Get(url)
+	if err != nil {
+		return nil, fmt.Errorf("Could not fetch %v, %v", url, err)
+	}
+	if res.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("Fetched status: %v", res.Status)
+	}
+
+	defer res.Body.Close()
+	b, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		return nil, fmt.Errorf("Unable to read contents")
+	}
+
+	return b, nil
 }
